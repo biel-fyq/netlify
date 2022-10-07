f4d860165da12a2734a5feeb721b2772605b9630
biel-fyq <3020882112@qq.com>
2022-10-07 10:47:31 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import 'antd/dist/antd.css'
+const rt = document.getElementById("root");
+
+ReactDOM.render(<App />, rt);
---
+++ src/testDemo.js
@@ -0,0 +1,8 @@
+
+const MobileImgTips = () => {
+    return (
+        <div style={{ textAlign: "center", fontSize: "12px", color: "#888" }}>点击图片可查看更多&gt;</div>
+    )
+}
+<MobileImgTips />
+// 159
